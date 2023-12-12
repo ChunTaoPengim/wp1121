3fb7a8b4bfb51b218a4959ffe91db75953dddfed
ChunTaoPengim <ChunTaoPengim@github.com>
2023-12-13 01:44:16 +0800

    fix

--- hack2/src/app/projects/_components/SignOutButton.tsx
+++ hack2/src/app/projects/_components/SignOutButton.tsx
@@ -5,7 +5,6 @@ import { signOut } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
 import { publicEnv } from "@/lib/env/public";
-import { useRouter } from "next/navigation";
 export default function SignOutButton() {
   const handlesignout = () => {
     signOut({ callbackUrl: `${publicEnv.NEXT_PUBLIC_BASE_URL}/auth`});
